60acabcf6f2bbfa3f5f6d5346d1bc2db680010e7
rizespbya <rizespb@ya.ru>
2025-09-27 09:34:44 +0300

    Fix: correct operation input type (#41)
    Closes #41

--- p1_calculator.py
+++ p1_calculator.py
@@ -1,20 +1,32 @@
-def calculator():
+"""Модуль p1_calculator.py.
+
+Простой консольный калькулятор, выполняющий базовые арифметические операции.
+"""
+
+
+def calculator() -> None:
+    """Простой консольный калькулятор.
+
+    Запрашивает два целых числа и арифметическую операцию. Выполняет операции +, -, *, /.
+    Выводит результат или сообщение об ошибке.
+    """
     no_1 = int(input("enter your first number"))
-    operation = int(input("enter  arithmetic operation  like +,-,*,/"))
+    operation = input("enter  arithmetic operation  like +,-,*,/")
     no_2 = int(input("enter your  second number"))
 
-    if operation =='+':
+    if operation == "+":
         print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
-    elif  operation =='-':
+    elif operation == "-":
         print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
-    elif operation =='*':
+    elif operation == "*":
         print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
-    elif  operation =='/':
+    elif operation == "/":
         if no_2 == 0:
             print("it gives infinity as it is not divided by zero")
         else:
             print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
     else:
         print("invalid output")
+
 
 calculator()
